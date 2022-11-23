4ec70d786357bdd1087fc48afa68fe5c7da1036a
jokatty <jyotikattani@gmail.com>
2022-11-23 10:10:33 +0530

    add tasks class to models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+  title = models.CharField(max_length=120)
+  description = models.CharField(max_length=1000)
+  completed = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.title
+
+
